601c10a1247480cd5d8c067d2f02632dd001d4ec
Khánh Duy <150520249+DuyNTK3020@users.noreply.github.com>
2025-08-04 07:54:13 +0700

    Add solutions for 1480. Running Sum of 1d Array
    Implemented the 'Running Sum of 1d Array' problem in both Java and JavaScript under the Easy section. Each solution computes the running sum of an input array as required by LeetCode problem 1480.

---
+++ ProblemList/Java/Easy/1480. Running Sum of 1d Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length];
+        result[0] = nums[0];
+        for (int i = 1; i < length; i++) {
+            result[i] = nums[i] + result[i - 1];
+        }
+        return result;
+    }
+}
